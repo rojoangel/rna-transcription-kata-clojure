1b6d7720c4a3c0813462778e4ea01b43553faa97
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-16 20:34:06 +0100

    G is transcribed in C

--- src/rna/core.clj
+++ src/rna/core.clj
@@ -1,1 +1,7 @@
 (ns rna.core)
+
+(def dna->rna
+  {\G \C})
+
+(defn transcribe [dna]
+  (map dna->rna dna))
--- test/rna/core_test.clj
+++ test/rna/core_test.clj
@@ -1,3 +1,7 @@
 (ns rna.core-test
   (:require [clojure.test :refer :all]
             [rna.core :refer :all]))
+
+(deftest rna-transcription-test
+  (testing "single element transcription"
+    (is (= [\C] (transcribe [\G])))))
