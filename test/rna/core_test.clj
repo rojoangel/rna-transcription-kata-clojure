78ffc4f07a57b8c7c2cd06bb415be2c7eed3ae13
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-16 20:40:36 +0100

    transcribes strands

--- test/rna/core_test.clj
+++ test/rna/core_test.clj
@@ -7,4 +7,10 @@
     (is (= [\C] (transcribe [\G])))
     (is (= [\G] (transcribe [\C])))
     (is (= [\A] (transcribe [\T])))
-    (is (= [\U] (transcribe [\A])))))
+    (is (= [\U] (transcribe [\A]))))
+
+  (testing "multiple element transcription"
+    (is (= [\C \C] (transcribe [\G \G])))
+    (is (= [\C \G] (transcribe [\G \C])))
+    (is (= [\C \G \A] (transcribe [\G \C \T])))
+    (is (= [\C \G \A \U] (transcribe [\G \C \T \A])))))
